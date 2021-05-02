247e659ce71e1f17283d9ed3bc335268816bd8e6
Petr Parolek <petr.parolek@gmail.com>
2021-05-02 14:14:55 +0200

    PHUnit -> Nette Tester

--- tests/cases/DI/Atlantic18BehaviorExtensionTest.php
+++ tests/cases/DI/Atlantic18BehaviorExtensionTest.php
@@ -1,19 +1,19 @@
 <?php declare(strict_types = 1);
 
-namespace Tests\Nettrine\Extensions\Atlantic18\Unit\DI;
+namespace Tests\Nettrine\Extensions\Atlantic18\DI;
 
 use Nette\DI\Compiler;
 use Nette\DI\Container;
 use Nette\DI\ContainerLoader;
 use Nettrine\Extensions\Atlantic18\DI\Atlantic18BehaviorExtension;
-use PHPUnit\Framework\TestCase;
+use Tester\Assert;
+use Tester\TestCase;
+
+require __DIR__ . '/../../bootstrap.php';
 
 final class Atlantic18BehaviorExtensionTest extends TestCase
 {
 
-	/**
-	 * @doesNotPerformAssertions
-	 */
 	public function testDefault(): void
 	{
 		$loader = new ContainerLoader(__DIR__ . '/../../tmp', true);
@@ -22,7 +22,10 @@ final class Atlantic18BehaviorExtensionTest extends TestCase
 		}, '1a');
 
 		$container = new $class();
-		assert($container instanceof Container);
+
+		Assert::type(Container::class, $container);
 	}
 
 }
+
+(new Atlantic18BehaviorExtensionTest())->run();
